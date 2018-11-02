fcb17966aa1e457350c2c92ec3a3aa4c89c59654
boriszn <boris.zaikin@re-flekt.com>
2018-11-02 08:53:04 +0100

    update file parsing

--- src/File.Import/Services/ExcelProcessingService.cs
+++ src/File.Import/Services/ExcelProcessingService.cs
@@ -6,6 +6,7 @@ using File.Import.Model;
 using Microsoft.AspNetCore.Http;
 using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace File.Import.Services
 {
@@ -19,8 +20,11 @@ namespace File.Import.Services
         {
             var users = new List<User>();
 
-            var hssfwb = new HSSFWorkbook(file.OpenReadStream());
-            ISheet sheet = hssfwb.GetSheetAt(0);
+            // var hssfwb = new HSSFWorkbook(file.OpenReadStream());
+
+            var xssfWorkbook = new XSSFWorkbook(file.OpenReadStream());
+
+            ISheet sheet = xssfWorkbook.GetSheetAt(0);
 
             IRow headerRow = sheet.GetRow(0); //Get Header Row
             int cellCount = headerRow.LastCellNum;
@@ -28,14 +32,23 @@ namespace File.Import.Services
             for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
             {
                 IRow row = sheet.GetRow(i);
-                if (row == null) continue;
-                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+
+                if (row == null)
+                {
+                    continue;
+                }
+
+                if (row.Cells.All(d => d.CellType == CellType.Blank))
+                {
+                    continue;
+                }
+
                 for (int j = row.FirstCellNum; j < cellCount; j++)
                 {
                     if (row.GetCell(j) != null)
                     {
                         // 
-                        row.GetCell(j).ToString();
+                        string cell = row.GetCell(j).ToString();
                     }
                 }
             }
